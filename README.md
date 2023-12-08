9d4b7969576d559b748d52b12851ea080167cdce
Thanh Nguyen <phapnguyen05032002@gmail.com>
2023-12-08 10:04:57 +0700

    code tailwind

---
+++ README.md
@@ -0,0 +1,6 @@
+- dùng lệnh dưới để build style cho code.
+
+npx tailwindcss -i ./src/style/style.css -o ./src/style/tailwind.css --watch
+
+- build code
+  npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-red-500">Đây là tailwindcss </h1>
+    </>
+  );
+}
+
+export default App;
